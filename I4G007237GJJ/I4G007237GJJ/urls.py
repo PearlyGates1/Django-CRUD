3a4ac9a558169daa9526fe8a41b49b0853a1feda
Pearl Okoroafor <pearlygatess01@gmail.com>
2022-07-03 19:05:56 +0100

    Initial commit

--- I4G007237GJJ/I4G007237GJJ/urls.py
+++ I4G007237GJJ/I4G007237GJJ/urls.py
@@ -1,4 +1,4 @@
-"""I4G002036D6U URL Configuration
+"""I4G007237GJJ URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
